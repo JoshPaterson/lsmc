026959f0127a783e8c3cc483911a017710d8e67f
Josh Paterson <mail@joshpaterson.com>
2022-04-03 19:35:03 -0400

    Many changes

--- lsmc_app/admin.py
+++ lsmc_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from treenode.admin import TreeNodeModelAdmin
 from treenode.forms import TreeNodeForm
-from .models import City, Book, Topic, CopyrightYear, Page, Section, Creator, Publisher, Printer, PageKind, Graphic
+from .models import City, Book, Topic, CopyrightYear, Page, Section, Creator, Publisher, Printer, PageKind, Graphic, Box
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
@@ -26,23 +26,26 @@ class GraphicKindAdmin(admin.ModelAdmin):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    inlines = [PageInline, SectionInline, GraphicInline]
+    # inlines = [PageInline, SectionInline, GraphicInline]
     date_hierarchy = 'date_published'
     fieldsets = (
         ('Title', {
-            'fields': (('title', 'title_checked'), ('subtitle', 'subtitle_checked'),('volume_number','volume_number_checked'), ('volume_number_kind', 'volume_number_kind_checked'), ('edition_number','edition_number_checked'))
+            'fields': (('title', 'title_checked'), ('subtitle', 'subtitle_checked'), ('volume_number','volume_number_checked'), ('volume_number_kind', 'volume_number_kind_checked'), ('issue_number','issue_number_checked'), ('issue_number_kind', 'issue_number_kind_checked'), ('edition_number','edition_number_checked'))
         }),
         ('Creators', {
             'fields': (('authors', 'editors', 'translators'), 'creators_checked')
         }),
+        ('Title/ Copyright Pages', {
+            'fields': (('title_page', 'title_page_checked'), ('copyright_page', 'copyright_page_checked'))
+        }),
         ('Topics', {
-            'fields': ('topics',)
+            'fields': ('topics', 'topics_checked')
         }),
         ('Book', {
-            'fields': (('has_ligatures', 'has_ligatures_checked'), ('scan_color', 'scan_color_checked'), ('numbers_offset', 'numbers_offset_checked'), ('roman_numbers_offset', 'roman_numbers_offset_checked'))
+            'fields': (('has_vector_text', 'has_vector_text_checked'), ('has_ligatures', 'has_ligatures_checked'), ('scan_color', 'scan_color_checked'), ('numbers_offset', 'numbers_offset_checked'), ('roman_numbers_offset', 'roman_numbers_offset_checked'))
         }),
         ('Other', {
-            'fields': ('document_id', 'url', 'downloaded_at')
+            'fields': ('slug', 'uuid', 'url', 'downloaded_at')
         }),
         ('Date', {
             'fields': (('date_published', 'date_published_checked'), ('in_copyright', 'in_copyright_checked'),('publishing_frequency', 'publishing_frequency_checked'))
@@ -54,6 +57,15 @@ class BookAdmin(admin.ModelAdmin):
             'fields': (('publishers', 'publishers_checked'), ('cities', 'cities_checked'))
         }),
     )
+    readonly_fields = ('slug', 'uuid', 'downloaded_at')
+    date_hierarchy = 'date_published'
+    filter_horizontal = ['authors', 'translators', 'editors', 'topics', 'copyright_years', 'printers', 'publishers', 'cities']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name in ['title_page', 'copyright_page']:
+            book_id = request.resolver_match.kwargs.get('object_id')
+            kwargs['queryset'] = Page.objects.filter(book=book_id)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 @admin.register(Topic)
 class TopicAdmin(TreeNodeModelAdmin):
@@ -68,6 +80,7 @@ class CopyrightYearAdmin(admin.ModelAdmin):
 class PageAdmin(admin.ModelAdmin):
     fields = ('image_tag',)
     readonly_fields = ('image_tag',)
+    list_display = ['book', 'number']
 
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
@@ -88,3 +101,11 @@ class PrinterAdmin(admin.ModelAdmin):
 @admin.register(PageKind)
 class PageKindAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(Box)
+class BoxAdmin(admin.ModelAdmin):
+    list_display = ('text', 'page_number', 'block_number', 'paragraph_number', 'line_number', 'word_number','level')
+    readonly_fields = ('page',)
+    list_filter = ['level', 'page_number', 'block_number', 'paragraph_number', 'line_number', 'word_number']
+    search_field = ['text', 'original_text']
+    empty_value_display = "''"
--- lsmc_app/models.py
+++ lsmc_app/models.py
@@ -4,7 +4,7 @@ from treenode.models import TreeNodeModel
 from django.utils.html import mark_safe
 
 class City(models.Model):
-    name = models.CharField(max_length=31)
+    name = models.CharField(max_length=31, unique=True)
     # books (MtM)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,69 +15,94 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/cities/{self.name}'
+
+
 
 class Topic(TreeNodeModel):
-    name = models.CharField(max_length=31)
+    name = models.CharField(max_length=31, unique=True)
     treenode_display_field = 'name'
-    # parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children')
     # books (MtM)
     # sections (MtM)
     # pages (MtM)
-    # children (OtM)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'/topics/{self.name}'
 
 
 class Creator(models.Model):
     # books (MtM)
     # sections (MtM)
-    name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=31)
+    given_names = models.CharField(max_length=31, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def get_full_name(self):
+        name = self.last_name
+        if self.given_names:
+            name = name + f', {self.given_names}'
+        return name
+
     def __str__(self):
-        return self.name
+        return self.get_full_name()
+
+    def get_absolute_url(self):
+        return f'/creators/{self.get_full_name()}'
 
 
 class Publisher(models.Model):
     # books (MtM)
-    name = models.CharField(max_length=63)
+    name = models.CharField(max_length=63, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'/publishers/{self.name}'
 
 
 class Printer(models.Model):
     # books (MtM)
-    name = models.CharField(max_length=63)
+    name = models.CharField(max_length=63, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/printers/{self.name}'
+
 
 class CopyrightYear(models.Model):
-    year = models.IntegerField()
+    year = models.IntegerField(unique=True)
     # books (MtM)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.year
+        return str(self.year)
+
+    def get_absolute_url(self):
+        return f'/copyright-years/{self.name}'
 
 
 class Page(models.Model):
     book = models.ForeignKey('Book', on_delete=models.CASCADE, related_name='pages')
-    number = models.PositiveIntegerField(blank=True, null=True)
+    number = models.PositiveSmallIntegerField(blank=True, null=True)
     topics = models.ManyToManyField(Topic, blank=True)
-    image = models.ImageField(upload_to='pages')
+    image = models.ImageField(upload_to='pages', unique=True)
     graphics = models.ManyToManyField('Graphic', blank=True)
+    text = models.TextField(blank=True)
     # sections (MtM)
     # section_headings (OtM)
     # kinds (MtM)
@@ -85,15 +110,17 @@ class Page(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{str(self.book)}: pg.{self.number}'
+        return f'pg. {self.number}'
 
     def image_tag(self):
-        return mark_safe(f'<img src="/{self.image}" />')
+        return mark_safe(f'<img src="{self.image.url}" />')
     image_tag.short_description = 'Image'
-    image_tag.allow_tags = True
 
     class Meta:
         ordering = ['number']
+
+    def get_absolute_url(self):
+        return f'{self.book.get_absolute_url()}/page/{self.number}'
 
 
 class Section(models.Model):
@@ -126,33 +153,40 @@ class Section(models.Model):
 
     book = models.ForeignKey('Book', on_delete=models.CASCADE, related_name='sections')
     kind = models.CharField(max_length=3, choices=Kind.choices, blank=True)
-    kind_checked = models.BooleanField(default=False, blank=True)
+    kind_checked = models.DateTimeField(blank=True, null=True)
     kind_in_book = models.CharField(max_length=15, blank=True)
-    kind_in_book_checked = models.BooleanField(default=False, blank=True)
+    kind_in_book_checked = models.DateTimeField(blank=True, null=True)
     title = models.CharField(max_length=100, blank=True)
-    title_checked = models.BooleanField(default=False, blank=True)
-    number = models.PositiveIntegerField(null=True, blank=True)
-    number_checked = models.BooleanField(default=False, blank=True)
+    title_checked = models.DateTimeField(blank=True, null=True)
+    number = models.PositiveSmallIntegerField(null=True, blank=True)
+    number_checked = models.DateTimeField(blank=True, null=True)
     number_kind = models.CharField(max_length=1, choices=NumberKind.choices, blank=True)
-    number_kind_checked = models.BooleanField(default=False, blank=True)
+    number_kind_checked = models.DateTimeField(blank=True, null=True)
     caption = models.TextField(blank=True)
-    caption_checked = models.BooleanField(default=False, blank=True)
-    for_edition = models.PositiveIntegerField(null=True, blank=True)
-    for_edition_checked = models.BooleanField(default=False, blank=True)
+    caption_checked = models.DateTimeField(blank=True, null=True)
+    for_edition = models.PositiveSmallIntegerField(null=True, blank=True)
+    for_edition_checked = models.DateTimeField(blank=True, null=True)
     heading_page = models.ForeignKey(Page, on_delete=models.RESTRICT, related_name='section_headings')
     pages = models.ManyToManyField(Page, blank=True)
-    pages_checked = models.BooleanField(default=False, blank=True)
+    first_page_checked = models.DateTimeField(blank=True, null=True)
+    last_page_checked = models.DateTimeField(blank=True, null=True)
     topics = models.ManyToManyField(Topic, blank=True)
-    topics_checked = models.BooleanField(default=False, blank=True)
+    topics_checked = models.DateTimeField(blank=True, null=True)
     authors = models.ManyToManyField(Creator, related_name='authored_sections', blank=True)
     editors = models.ManyToManyField(Creator, related_name='edited_sections', blank=True)
     translators = models.ManyToManyField(Creator, related_name='translated_sections', blank=True)
-    creators_checked = models.BooleanField(default=False, blank=True)
+    creators_checked = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{str(self.book)} {self.kind.capitalize()} {self.number}'
+
+    def get_absolute_url(self):
+        if self.kind & self.number:
+            return f'{self.book.get_absolute_url()}/{self.kind}/{self.number}'
+        else:
+            return f'{self.book.get_absolute_url()}/section/{self.id}'
 
     class Meta:
         ordering = ['heading_page']
@@ -172,71 +206,101 @@ class Book(models.Model):
         GRAYSCALE = 'GRA'
 
     class PublishingFrequency(models.TextChoices):
+        ONCE = 'O'
         YEARLY = 'Y'
         QUARTERLY = 'Q'
         MONTHLY = 'M'
         WEEKLY = 'W'
         DAILY = 'D'
 
-    document_id = models.UUIDField(max_length=50, null=True, blank=True)
-    url = models.URLField(blank=True)
+    uuid = models.UUIDField(max_length=36, null=True, blank=True, unique=True)
+    url = models.URLField(blank=True, null=True)
     downloaded_at = models.DateField(null=True, blank=True)
     date_published = models.DateField(null=True, blank=True)
-    date_published_checked = models.BooleanField(default=False, blank=True)
+    date_published_checked = models.DateTimeField(blank=True, null=True)
     publishing_frequency = models.CharField(max_length=1, choices=PublishingFrequency.choices, blank=True)
-    publishing_frequency_checked = models.BooleanField(default=False, blank=True)
+    publishing_frequency_checked = models.DateTimeField(blank=True, null=True)
     scan_color = models.CharField(max_length=3, choices=Color.choices, blank=True)
-    scan_color_checked = models.BooleanField(default=False, blank=True)
+    scan_color_checked = models.DateTimeField(blank=True, null=True)
+    title_page = models.OneToOneField(Page, related_name='title_page_for', on_delete=models.SET_NULL, blank=True, null=True)
+    title_page_checked = models.DateTimeField(blank=True, null=True)
+    copyright_page = models.OneToOneField(Page, related_name='copyright_page_for', on_delete=models.SET_NULL, blank=True, null=True)
+    copyright_page_checked = models.DateTimeField(blank=True, null=True)
+    has_vector_text = models.BooleanField(null=True)
+    has_vector_text_checked = models.DateTimeField(blank=True, null=True)
     title = models.CharField(max_length=100, blank=True)
-    title_checked = models.BooleanField(default=False, blank=True)
+    title_checked = models.DateTimeField(blank=True, null=True)
     subtitle = models.CharField(max_length=100, blank=True)
-    subtitle_checked = models.BooleanField(default=False, blank=True)
-    edition_number = models.PositiveIntegerField(null=True, blank=True)
-    edition_number_checked = models.BooleanField(default=False, blank=True)
-    volume_number = models.PositiveIntegerField(null=True, blank=True)
-    volume_number_checked = models.BooleanField(default=False, blank=True)
+    subtitle_checked = models.DateTimeField(blank=True, null=True)
+    edition_number = models.PositiveSmallIntegerField(null=True, blank=True)
+    edition_number_checked = models.DateTimeField(blank=True, null=True)
+    volume_number = models.PositiveSmallIntegerField(null=True, blank=True)
+    volume_number_checked = models.DateTimeField(blank=True, null=True)
     volume_number_kind = models.CharField(max_length=1, choices=NumberKind.choices, blank=True)
-    volume_number_kind_checked = models.BooleanField(default=False, blank=True)
-    printing_number = models.PositiveIntegerField(null=True, blank=True)
-    printing_number_checked = models.BooleanField(default=False, blank=True)
-    numbers_offset = models.PositiveIntegerField(null=True, blank=True)
-    numbers_offset_checked = models.BooleanField(default=False, blank=True)
-    roman_numbers_offset = models.PositiveIntegerField(null=True, blank=True)
-    roman_numbers_offset_checked = models.BooleanField(default=False, blank=True)
+    volume_number_kind_checked = models.DateTimeField(blank=True, null=True)
+    issue_number = models.PositiveSmallIntegerField(null=True, blank=True)
+    issue_number_checked = models.DateTimeField(blank=True, null=True)
+    issue_number_kind = models.CharField(max_length=1, choices=NumberKind.choices, blank=True)
+    issue_number_kind_checked = models.DateTimeField(blank=True, null=True)
+    printing_number = models.PositiveSmallIntegerField(null=True, blank=True)
+    printing_number_checked = models.DateTimeField(blank=True, null=True)
+    numbers_offset = models.PositiveSmallIntegerField(null=True, blank=True)
+    numbers_offset_checked = models.DateTimeField(blank=True, null=True)
+    roman_numbers_offset = models.PositiveSmallIntegerField(null=True, blank=True)
+    roman_numbers_offset_checked = models.DateTimeField(blank=True, null=True)
     in_copyright = models.BooleanField(null=True, blank=True)
-    in_copyright_checked = models.BooleanField(default=False, blank=True)
+    in_copyright_checked = models.DateTimeField(blank=True, null=True)
     has_ligatures = models.BooleanField(null=True, blank=True)
-    has_ligatures_checked = models.BooleanField(default=False, blank=True)
+    has_ligatures_checked = models.DateTimeField(blank=True, null=True)
     cities = models.ManyToManyField(City, blank=True)
-    cities_checked = models.BooleanField(default=False, blank=True)
+    cities_checked = models.DateTimeField(blank=True, null=True)
     topics = models.ManyToManyField(Topic, blank=True)
-    topics_checked = models.BooleanField(default=False, blank=True)
+    topics_checked = models.DateTimeField(blank=True, null=True)
     authors = models.ManyToManyField(Creator, related_name='authored_books', blank=True)
     editors = models.ManyToManyField(Creator, related_name='edited_books', blank=True)
     translators = models.ManyToManyField(Creator, related_name='translated_books', blank=True)
-    creators_checked = models.BooleanField(default=False, blank=True)
+    creators_checked = models.DateTimeField(blank=True, null=True)
     printers = models.ManyToManyField(Printer, blank=True)
-    printers_checked = models.BooleanField(default=False, blank=True)
+    printers_checked = models.DateTimeField(blank=True, null=True)
     publishers = models.ManyToManyField(Publisher, blank=True)
-    publishers_checked = models.BooleanField(default=False, blank=True)
+    publishers_checked = models.DateTimeField(blank=True, null=True)
     copyright_years = models.ManyToManyField(CopyrightYear, blank=True)
-    copyright_years_checked = models.BooleanField(default=False, blank=True)
+    copyright_years_checked = models.DateTimeField(blank=True, null=True)
     # pages (OtM)
     # sections (OtM)
-    sections_checked = models.BooleanField(default=False, blank=True)
+    sections_checked = models.DateTimeField(blank=True, null=True)
     # graphics (OtM)
-    graphics_checked = models.BooleanField(default=False, blank=True)
+    graphics_checked = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        string = f'{self.date_published.year}: {self.title}'
-        if self.volume_number:
-            string += f' Vol. {self.volume_number}'
-        return string
+        # string = f'{self.date_published.year}: {self.title}'
+        # if self.volume_number:
+            # string += f' Vol. {self.volume_number}'
+        return str(self.uuid)
+
+    def get_first_creator(self):
+        creators = [a.last_name for a in self.authors] + [e.last_name for e in self.editors] + [t.last_name for t in self.translators] + ''
+        return creators[0]
+
+    def make_slug(self):
+        slug = str(self.date_published.year)
+        if creator:=self.get_first_creator():
+            slug = slug + f'-{creator}'
+        if self.title:
+            slug = slug + f'-{self.title}'
+        return slug
+
+    def get_absolute_url(self):
+        if self.slug:
+            return f'books/{self.slug}'
+        else:
+            return f'books/{self.uuid}'
 
     class Meta:
-        pass
+        get_latest_by = 'date_published'
 
 
 class PageKind(models.Model):
@@ -259,12 +323,15 @@ class PageKind(models.Model):
         GRAPHICS = 'GRA'
 
     pages = models.ManyToManyField(Page, blank=True)
-    name = models.CharField(max_length=3, choices=Kind.choices)
+    name = models.CharField(max_length=3, choices=Kind.choices, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'page-kind/{self.name}'
 
 
 class Graphic(models.Model):
@@ -281,6 +348,8 @@ class Graphic(models.Model):
         TECHNICAL_DRAWING = 'TEC'
         DRAWING = 'DRA'
         DECORATION = 'DEC'
+        EQUATION = 'EQU'
+        MUSIC_NOTATION = 'MUS'
 
     class Color(models.TextChoices):
         COLOR = 'COL'
@@ -289,16 +358,53 @@ class Graphic(models.Model):
 
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='graphics')
     medium = models.CharField(max_length=3, choices=Medium.choices, blank=True)
-    medium_checked = models.BooleanField(default=False, blank=True)
+    medium_checked = models.DateTimeField(blank=True, null=True)
     content = models.CharField(max_length=3, choices=Content.choices, blank=True)
-    content_checked = models.BooleanField(default=False, blank=True)
+    content_checked = models.DateTimeField(blank=True, null=True)
     print_color = models.CharField(max_length=3, choices=Color.choices, blank=True)
-    print_color_checked = models.BooleanField(default=False, blank=True)
+    print_color_checked = models.DateTimeField(blank=True, null=True)
     # pages (MtM)
-    pages_checked = models.BooleanField(default=False, blank=True)
+    pages_checked = models.DateTimeField(blank=True, null=True)
     artists = models.ManyToManyField(Creator)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.kind} on pg. {self.pages[0].number} of {self.book}'
+
+    def get_absolute_url(self):
+        return f'{self.book.get_absolute_url()}/graphic/{self.id}'
+
+class Box(models.Model):
+    class Level(models.IntegerChoices):
+        PAGE = 1
+        BLOCK = 2
+        PARAGRAPH = 3
+        LINE = 4
+        WORD = 5
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, blank=True)
+    original_text = models.CharField(max_length=32, blank=True, editable=False)
+    text = models.CharField(max_length=32, blank=True)
+    level = models.PositiveSmallIntegerField(choices=Level.choices)
+    page_number = models.PositiveSmallIntegerField()
+    block_number = models.PositiveSmallIntegerField()
+    paragraph_number = models.PositiveSmallIntegerField()
+    line_number = models.PositiveSmallIntegerField()
+    word_number = models.PositiveSmallIntegerField()
+    left = models.PositiveSmallIntegerField()
+    top = models.PositiveSmallIntegerField()
+    width = models.PositiveSmallIntegerField()
+    height = models.PositiveSmallIntegerField()
+    original_confidence = models.FloatField(blank=True, null=True, editable=False)
+    confidence = models.FloatField(blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'boxes'
+
+    def __str__(self):
+        return f'{self.page_number}:{self.block_number}:{self.paragraph_number}:{self.line_number}:{self.word_number}:{self.text}'
+
+    def get_absolute_url(self):
+        return f'box/{self.id}'
