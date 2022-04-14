1d407253b86803ea50c1a6e767b427958e7b5e9c
Josh Paterson <mail@joshpaterson.com>
2022-04-13 23:14:03 -0400

    Get basic view working

--- lsma/urls.py
+++ lsma/urls.py
@@ -1,7 +1,7 @@
 from operator import ge
 from django.urls import path
-from .views import generate_box_image
+from . import views
 
 urlpatterns = [
-    path('boxes/<str:book_slug>/<int:page>/<str:level>/<int:page_number>/<int:block_number>/<int:paragraph_number>/<int:line_number>/<int:word_number>', generate_box_image)
+    path('', views.index)
 ]
